8977a18c68c82e022940daa37d33e70c8b906230
XmeskoA <xmeskoa@stuba.sk>
2023-12-03 00:41:34 +0100

    funguje to

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.net.SocketOption;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
- public void main () {
-     System.out.println(69);
+ public static void main (String[] args) {
+     System.out.println(75);
  }
 }
