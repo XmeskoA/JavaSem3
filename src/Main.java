83acc8752c97c942f992eab4f3deaeee0f43cfa5
XmeskoA <xmeskoa@stuba.sk>
2023-12-03 00:49:26 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.net.SocketOption;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
  public static void main (String[] args) {
-     System.out.println(75);
+
  }
 }
